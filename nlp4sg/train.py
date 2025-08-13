5f87fe9289296bf21efced608b70d31cf494e733
gregorlied <lied@tu-berlin.de>
2025-08-13 19:42:58 +0200

    Fix default config

--- nlp4sg/train.py
+++ nlp4sg/train.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--config", 
         type=str, 
-        default= "./configs/flan_t5_base.yaml",
+        default= "./configs/config.yaml",
     )
     args = parser.parse_args()
 
